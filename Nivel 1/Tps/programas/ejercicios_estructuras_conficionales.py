e3bf964150f1bf668d66f26988473161abebad12
GinoHumberto <deldealavista@gmail.com>
2025-09-24 23:53:02 -0300

    Avances en el bucle while, comienzo del for

--- Nivel 1/Tps/programas/ejercicios_estructuras_conficionales.py
+++ Nivel 1/Tps/programas/ejercicios_estructuras_conficionales.py
@@ -218,9 +218,234 @@ else:
 #menor de ellos)
 
 #considerando la variacion como la diferencia:
+'''
+def dif (num1, num2):
+    dif = num1 - num2
+    if dif < 0:
+        dif = dif*-1
+    return(dif)
+
 valores = []
 while len(valores) < 3:
     valor = int(input('Ingresa tu valor: '))
     valores.append(valor)
 print('Tus valores son', valores)
 
+dif1 = dif(valores[1], valores[2])
+dif2 = dif(valores[0], valores[1])
+dif3 = dif(valores[0], valores[2])
+print(dif1, dif2, dif3)
+
+#mayor:
+if dif1 > dif2 and dif1 > dif3:
+    print(f'la mayor diferencia es {dif1} que es la diferencia ente {valores[1], valores[2]}')
+elif dif2 > dif1 and dif2 > dif3:
+    print(f'la mayor diferencia es {dif2} que es la diferencia ente {valores[0], valores[1]}')
+else:
+    print(f'la mayor diferencia es {dif3} que es la diferencia ente {valores[0], valores[2]}')
+#menor:
+if dif1 < dif2 and dif1 < dif3:
+    print(f'la menor diferencia es {dif1} que es la diferencia ente {valores[1], valores[2]}')
+elif dif2 < dif1 and dif2 < dif3:
+    print(f'la menor diferencia es {dif2} que es la diferencia ente {valores[0], valores[1]}')
+else:
+    print(f'la menor diferencia es {dif3} que es la diferencia ente {valores[0], valores[2]}')
+'''
+#Escribir un programa que solicite ingresar 10 notas de alumnos y nos informe
+#cuántos tienen notas mayores o iguales a 7 y cuántos menores.
+'''
+mayor7 = 0
+menor7 = 0
+operaciones = 0 
+while operaciones < 10:
+    solicitud = int(input('Ingresa la nota: '))
+    while solicitud < 0 or solicitud > 10:
+        solicitud = int(input('Debe ser mayor que 0 y menor a 10: '))
+    if solicitud >= 7:
+        mayor7 += 1
+    else:
+        menor7 += 1
+    operaciones += 1
+print(f'Hay {mayor7} alumnos con notas mayores a 7 y {menor7} con notas menores a 7')
+'''
+#Se ingresan un conjunto de n alturas de personas por teclado. Mostrar la
+#altura promedio de las personas.
+'''
+alturas = []
+n = int(input('Ingresa cuanta gente va a ser parte del estudio: '))
+while n < 0:
+    n = int(input('Ingresa cuanta gente va a ser parte del estudio: '))
+while len(alturas) != n:
+    altura = int(input('Ingresa la altura: '))
+    alturas.append(altura)
+print(f'El promedio de alturas es {sum(alturas)/n}')
+'''
+#En una empresa trabajan n empleados cuyos sueldos oscilan entre $100 y
+#$500, realizar un programa que lea los sueldos que cobra cada empleado e
+#informe cuántos empleados cobran entre $100 y $300 y cuántos cobran más
+#de $300. Además el programa deberá informar el importe que gasta la
+#empresa en sueldos al personal.
+'''
+#Toma de datos
+n = int(input('Ingresa cuanta gente va a ser parte del estudio: '))
+while n < 0:
+    n = int(input('Ingresa cuanta gente va a ser parte del estudio: '))
+#calculo de sueldos
+operaciones = 0
+sueldos_altos = 0
+sueldos_bajos = 0
+gasto_total = 0
+while operaciones != n:
+    sueldo = int(input('Ingresa el sueldo del empleado: '))
+    while sueldo < 100 or sueldo > 500:
+        sueldo = int(input('El sueldo no puede ser un valor atipico, ingresa un sueldo verdadero: '))
+    if sueldo > 300:
+        sueldos_altos += 1
+    else:
+        sueldos_bajos += 1
+    gasto_total += sueldo
+    operaciones += 1
+print(f'Hay {sueldos_altos} personas con sueldo alto, hay {sueldos_bajos} personas con sueldo bajo y la empresa gasta: {gasto_total}')
+'''
+#Realizar un programa que imprima 25 términos de la serie 11 - 22 - 33 - 44,
+#etc. (No se ingresan valores por teclado)
+'''
+n = 25
+times = 0
+number = 0
+while n != times:
+    if number < 99:
+        number += 11
+    elif number > 100 and number < 999 :
+        number += 111
+    elif number == 999:
+        number += 112
+    elif number > 999:
+        number += 1111
+    else:
+        number += 12
+    times += 1
+    print(number)
+'''
+#Mostrar los múltiplos de 8 hasta el valor 500. Debe aparecer en pantalla 8 - 16
+#- 24, etc.
+'''
+calculo = 0
+multiplo = 1
+while calculo < 500:
+    calculo = multiplo * 8
+    multiplo += 1
+    if calculo < 500:
+        print(calculo)
+'''
+#Realizar un programa que permita cargar dos listas de 15 valores cada una.
+#Informar con un mensaje cual de las dos listas tiene un valor acumulado
+#mayor (mensajes "Lista 1 mayor", "Lista 2 mayor", "Listas iguales")
+#Tener en cuenta que puede haber dos o más estructuras repetitivas en un
+#algoritmo.
+'''
+def entrada(rango):
+    n = 0
+    lista = []
+    while n < rango:
+        valor = int(input('Ingresa tus valores '))
+        while valor < 0:
+            valor = int(input('Debe ser positivo '))
+        lista.append(valor)
+        n += 1
+    return(lista)
+
+print('Primera lista')
+lista1 = entrada(15)
+print(lista1)
+print('Segunda lista')
+lista2 = entrada(15)
+print(lista2)
+
+if sum(lista1) > sum(lista2):
+    print('La lista 1 es mayor')
+elif sum(lista1) == sum(lista2):
+    print('Ambas listas son iguales')
+else:
+    print('La lista 2 es mayot')
+'''
+#Desarrollar un programa que permita cargar n números enteros y luego nos
+#informe cuántos valores fueron pares y cuántos impares.
+#Emplear el operador “%” en la condición de la estructura condicional (este
+#operador retorna el resto de la división de dos valores, por ejemplo 11%2
+#retorna un 1):
+#if valor%2==0:
+'''
+q_valores = int(input('Ingresa cuantos numeros enteros vas a cargar '))
+while q_valores < 0:
+    q_valores = int(input('El número debe ser positivo '))
+
+impares = 0
+pares = 0
+n = 0
+
+while n != q_valores:
+    valor = int(input('Ingresa tu valor '))
+    if valor %2 == 0:
+        pares += 1
+    else:
+        impares += 1
+    n += 1
+print(f'ingresaste {impares} valor(es) impares y {pares} pares')
+'''
+# Confeccionar un programa que lea n pares de datos, cada par de datos
+# corresponde a la medida de la base y la altura de un triángulo. El programa
+# deberá informar:
+# a) De cada triángulo la medida de su base, su altura y su superficie.
+# b) La cantidad de triángulos cuya superficie es mayor a 12.
+
+
+
+# Desarrollar un programa que solicite la carga de 10 números e imprima la
+# suma de los últimos 5 valores ingresados.
+
+
+
+# Desarrollar un programa que muestre la tabla de multiplicar del 5 (del 5 al 50)
+
+
+
+# Confeccionar un programa que permita ingresar un valor del 1 al 10 y nos
+# muestre la tabla de multiplicar del mismo (los primeros 12 términos)
+# Ejemplo: Si ingreso 3 deberá aparecer en pantalla los valores 3, 6, 9, hasta el
+# 36.
+
+
+
+# Realizar un programa que lea los lados de n triángulos, e informar:
+# a) De cada uno de ellos, qué tipo de triángulo es: equilátero (tres lados
+# iguales), isósceles (dos lados iguales), o escaleno (ningún lado igual)
+# b) Cantidad de triángulos de cada tipo.
+
+
+
+# Escribir un programa que pida ingresar coordenadas (x,y) que representan
+# puntos en el plano.
+# Informar cuántos puntos se han ingresado en el primer, segundo, tercer y
+# cuarto cuadrante. Al comenzar el programa se pide que se ingrese la cantidad
+# de puntos a procesar.
+
+
+
+# Se realiza la carga de 10 valores enteros por teclado. Se desea conocer:
+# a) La cantidad de valores ingresados negativos.
+# b) La cantidad de valores ingresados positivos.
+# c) La cantidad de múltiplos de 15.
+# d) El valor acumulado de los números ingresados que son pares.
+
+
+
+# Se cuenta con la siguiente información:
+# Las edades de 5 estudiantes del turno mañana
+# Las edades de 6 estudiantes del turno tarde.
+# Las edades de 11 estudiantes del turno noche.
+# Las edades de cada estudiante deben ingresarse por teclado.
+# a) Obtener el promedio de las edades de cada turno (tres promedios)
+# b) Imprimir dichos promedios (promedio de cada turno)
+# c) Mostrar por pantalla un mensaje que indique cual de los tres turnos tiene un
+# promedio de edades mayor.
